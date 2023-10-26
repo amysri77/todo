07175e01c55ef3994a9ef7bc572573dce8b55e69
amrita srivastava <amrita1662001@gmail.com>
2023-10-26 13:46:59 +0530

    again commit

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  mobile: { type: Number },
+  mobile: { type: Number }, 
   image: {type:String},
 },{
   timestamps: true
